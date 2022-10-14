f3bbec550d3082ece22555120c12a8a04d639eaf
Chiwan Park <skqkrcldhks@gmail.com>
2022-10-14 16:27:16 +0200

    파이보 프로젝트 최초 커밋

---
+++ pybo/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from pybo.models import Question, Answer
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question  # 사용할 모델
+        fields = ['subject', 'content']  # QuestionForm에서 사용할 Question 모델의 속성
+        labels = {
+            'subject': 'Titel',
+            'content': 'Inhalts',
+        }  
+        
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
+        }
